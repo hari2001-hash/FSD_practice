f31c90a584e13eaacae1fd60fd6be1651cf49c6e
hari2001-hash <hh6995161@gmail.com>
2025-06-28 16:46:09 +0530

    Third commit1

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ app.use(express.json());
 const userRoutes = require("./routes/userRoutes");
 app.use("/api", userRoutes);
 
-
-
 // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 //   .then(() => console.log("MongoDB connected"))
 //   .catch(err => console.log("MongoDB error:", err));
-// moved this code to config folder-->db.js
+// moved this code to config folder-->dv.js
   const connectDB = require("./config/db");
 connectDB();
 
